e02c69f6c29010a92daa8f7fa678136b71007332
weqpro <weqpro@users.noreply.github.com>
2021-01-01 02:10:49 +0200

    h

---
+++ VisStud/Cons/Cons/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OOOO");
+            Console.ReadKey();
+        }
+    }
+}
